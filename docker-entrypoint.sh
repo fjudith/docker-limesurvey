e6de4ba98756c033b5c5d3554f7e0280563257f3
JUDITH Florian <florian.judith@economat-armees.fr>
2017-09-09 13:50:27 +0200

    updated docker-entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,25 +1,54 @@
 #!/bin/bash
 set -e 
 
-SMTP_HOST=${SMTP_HOST:-localhost}
-SMTP_PORT=${SMTP_PORT:-25}
-SMTP_PROTOCOL=${MAIL_PROTOCOL:-smtp}
-SMTP_AUTH=${SMTP_AUTH:-off}
+function cfg_replace_option {
+  grep "$1" "$3" > /dev/null
+  if [ $? -eq 0 ]; then
+    # replace option
+    echo "replacing option  $1=$2  in  $3"
+    sed -i "s#^\($1\s*=\s*\).*\$#\1$2#" $3
+    if (( $? )); then
+      echo "cfg_replace_option failed"
+      exit 1
+    fi
+  else
+    # add option if it does not exist
+    echo "adding option  $1=$2  in  $3"
+    echo "$1=$2" >> $3
+  fi
+}
+
+SMTP_HOST=${SMTP_HOST:-'localhost'}
+SMTP_PORT=${SMTP_PORT:-'25'}
+SMTP_PROTOCOL=${MAIL_PROTOCOL:-'smtp'}
+SMTP_AUTH=${SMTP_AUTH:-'off'}
 SMTP_USERNAME=${SMTP_USERNAME:-}
 SMTP_PASSWORD=${SMTP_PASSWORD:-}
 
-SMTP_TLS=${SMTP_TLS:-off}
-SMTP_STARTTLS=${SMPTP_STARTTLS_ENABLE:-off}
-SMTP_TLS_CHECK=${SMTP_TLS_CHECK:-off}
+SMTP_TLS=${SMTP_TLS:-'off'}
+SMTP_STARTTLS=${SMPTP_STARTTLS_ENABLE:-'off'}
+SMTP_TLS_CHECK=${SMTP_TLS_CHECK:-'off'}
 SMTP_TLS_TRUST_FILE=${SMTP_TLS_TRUST_FILE:-}
 
-MAIL_FROM_DEFAULT=${MAIL_FROM_DEFAULT:-limesurvey@example.com}
+MAIL_FROM_DEFAULT=${MAIL_FROM_DEFAULT:-'limesurvey@example.com'}
 MAIL_TRUST_DOMAIN=${MAIL_TRUST_DOMAIN:-}
 MAIL_DOMAIN=${MAIL_DOMAIN:-}
-SMTP_TIMEOUT=${SMTP_TIMEOUT:-30000}
-MAIL_SMTP_DEBUG=${MAIL_SMTP_DEBUG:-false}
+SMTP_TIMEOUT=${SMTP_TIMEOUT:-'30000'}
 
 PUBLIC_URL=${PUBLIC_URL:-}
+
+DB_KIND=${DB_KIND:-'mysql'}
+DB_HOST=${DB_HOST:-'mysql'}
+DB_PORT=${DB_PORT:-'3306'}
+DB_NAME=${DB_NAME:-'limesurvey'}
+DB_CHARSET=${DB_CHARSET:-'utf8mb4'}
+DB_TABLE_PREFIX=${$DB_TABLE_PREFIX:-'lime_'}
+DB_USERNAME=${DB_USERNAME:-}
+DB_PASSWORD=${DB_PASSWORD:-}
+
+MEMCACHED_HOST=${MEMCACHED_HOST:-}
+MEMCACHED_PORT=${MEMCACHED_PORT:-11211}
+MEMCACHED_WEIGHT${MEMCACHED_PORT:-100}
 
 # Write MSMTP configuration
 cat > /etc/msmtprc << EOL
@@ -41,8 +70,13 @@ timeout ${SMTP_TIMEOUT}
 EOL
 
 # Write Public URL
-if [ -v "$PUBLIC_URL" ]; then
-    sed -i "s#\('debug'=>0,\)\$#\\1 'publicurl'=>'${PUBLIC_URL}',#g" application/config/config.php
+if [ "$PUBLIC_URL" ]; then
+    sed -i "s#\('debug'=>0,\)\$#'publicurl'=>'${PUBLIC_URL}',\n\t\t\\1 #g" application/config/config.php
+fi
+
+# Write Memcached config
+if [ "$MEMCACHED_HOST"]; then
+    sed -i "s#\('db' => array(\)#'cache'=>array(\n\t\t\t'class'=>'CMemCache',\n\t\t\t'servers'=>array(\n\t\t\t\tarray(\n\t\t\t\t\t'host'=>'${MEMCACHED_HOST}',\n\t\t\t\t\t'port'=>'${MEMCACHED_PORT}',\n\t\t\t\t\t'weight'=>'${MEMCACHED_WEIGHT}',\n\t\t\t\t),\n\t\t\t),\n\t\t),\n\t\t\\1 #g" application/config/config.php
 fi
 
 # Start Aphache
