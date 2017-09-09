831a98bdc5bf959af0f30383b4c7c11c47c2b93a
JUDITH Florian <florian.judith@economat-armees.fr>
2017-09-09 17:19:10 +0200

    Added support mysql en postgres db linking

--- fpm/docker-entrypoint.sh
+++ fpm/docker-entrypoint.sh
@@ -20,11 +20,10 @@ SMTP_TIMEOUT=${SMTP_TIMEOUT:-'30000'}
 
 PUBLIC_URL=${PUBLIC_URL:-}
 
-DB_KIND=${DB_KIND:-'mysql'}
+DB_TYPE=${DB_TYPE:-'mysql'}
 DB_HOST=${DB_HOST:-'mysql'}
 DB_PORT=${DB_PORT:-'3306'}
 DB_NAME=${DB_NAME:-'limesurvey'}
-DB_CHARSET=${DB_CHARSET:-'utf8mb4'}
 DB_TABLE_PREFIX=${DB_TABLE_PREFIX:-'lime_'}
 DB_USERNAME=${DB_USERNAME:-}
 DB_PASSWORD=${DB_PASSWORD:-}
@@ -55,7 +54,51 @@ timeout ${SMTP_TIMEOUT}
 EOL
 
 # Write Database config
-sed -i "s#\('connectionString' => \).*,\$#\\1'${DB_KIND}:host=${DB_HOST};port=${DB_PORT};dbname=${DB_NAME};',#g" application/config/config.php
+# if we're linked to MySQL and thus have credentials already, let's use them
+if [[ -v MYSQL_ENV_GOSU_VERSION ]]; then
+    : ${DB_TYPE='mysql'}
+    : ${DB_USERNAME:=${MYSQL_ENV_MYSQL_USER:-root}}
+    if [ "$DB_USERNAME" = 'root' ]; then
+        : ${DB_PASSWORD:=$MYSQL_ENV_MYSQL_ROOT_PASSWORD}
+    fi
+    : ${DB_PASSWORD:=$MYSQL_ENV_MYSQL_PASSWORD}
+    : ${DB_NAME:=${MYSQL_ENV_MYSQL_DATABASE:-limesurvey}}
+    : ${DB_CHARSET=${DB_CHARSET:-'utf8mb4'}}
+
+    if [ -z "$DB_PASSWORD" ]; then
+        echo >&2 'error: missing required DB_PASSWORD environment variable'
+        echo >&2 '  Did you forget to -e DB_PASSWORD=... ?'
+        echo >&2
+        echo >&2 '  (Also of interest might be DB_USERNAME and DB_NAME.)'
+        exit 1
+    fi
+
+    cp application/config/config-sample-mysql.php application/config/config.php
+fi
+
+# if we're linked to PostgreSQL and thus have credentials already, let's use them
+if [[ -v POSTGRES_ENV_GOSU_VERSION ]]; then
+    : ${DB_TYPE='postgresql'}
+    : ${DB_USERNAME:=${POSTGRES_ENV_POSTGRES_USER:-root}}
+    if [ "$DB_USERNAME" = 'postgres' ]; then
+        : ${DB_PASSWORD:='postgres' }
+    fi
+    : ${DB_PASSWORD:=$POSTGRES_ENV_POSTGRES_PASSWORD}
+    : ${DB_NAME:=${POSTGRES_ENV_POSTGRES_DB:-limesurvey}}
+    : ${DB_CHARSET=${DB_CHARSET:-'utf8'}}
+
+    if [ -z "$DB_PASSWORD" ]; then
+        echo >&2 'error: missing required DB_PASSWORD environment variable'
+        echo >&2 '  Did you forget to -e DB_PASSWORD=... ?'
+        echo >&2
+        echo >&2 '  (Also of interest might be DB_USERNAME and DB_NAME.)'
+        exit 1
+    fi
+
+    cp application/config/config-sample-pgsql.php application/config/config.php
+fi
+
+sed -i "s#\('connectionString' => \).*,\$#\\1'${DB_TYPE}:host=${DB_HOST};port=${DB_PORT};dbname=${DB_NAME};',#g" application/config/config.php
 sed -i "s#\('username' => \).*,\$#\\1'${DB_USERNAME}',#g" application/config/config.php
 sed -i "s#\('password' => \).*,\$#\\1'${DB_PASSWORD}',#g" application/config/config.php
 sed -i "s#\('charset' => \).*,\$#\\1'${DB_CHARSET}',#g" application/config/config.php
